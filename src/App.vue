c8370c441b7ab92db2219991c3a038dc9197bc8c
MykolaDekusar <ndekusar97@gmail.com>
2024-07-30 15:40:20 +0200

    [UPD] Milestone 4 completed, successfully retrieved Projects data from server

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { store } from "../src/store";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      projects: null,
+      currentPage: 1,
+      lastPage: null,
+      store,
+    };
+  },
+
+  mounted() {
+    axios
+      .get(this.store.serverIp, {
+        params: {
+          page: this.currentPage,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results.data);
+        this.projects = response.data.results.data;
+      });
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
